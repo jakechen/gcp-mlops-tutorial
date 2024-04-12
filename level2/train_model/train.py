506b21e6b52830a1c39c4b8c64b4de71f1efe4b1
Jake Chen (Google) <jakejchen@google.com>
2024-04-12 19:53:59 +0000

    push to gcs

--- level2/train_model/train.py
+++ level2/train_model/train.py
@@ -1,7 +1,9 @@
-# Import the necessary libraries
+import os
+import pickle
+from google.cloud import storage
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-import pickle
+
 
 # Load the data
 iris = load_iris()
@@ -13,4 +15,11 @@ model.fit(iris.data, iris.target)
 # Save the model to a file
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
+
+# Push saved model to GCS bucket
+storage_client = storage.Client()
+bucket = storage_client.bucket(os.environ['AIP_MODEL_DIR'])
+blob = bucket.blob('model.pkl')
+blob.upload_from_filename('model.pkl')
+
 print("Training completed.")
