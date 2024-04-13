7628d696fb5400131e8d73aacda93826bc4178d0
Jake Chen (Google) <jakejchen@google.com>
2024-04-13 21:59:49 +0000

    change to cloud run, add kfp repo

--- level2/stack.tf
+++ level2/stack.tf
@@ -1,3 +1,4 @@
+# Stack fundamentals
 locals {
   project = "simple-pipeline-415719"
   location = "us-west1"
@@ -23,42 +24,51 @@ resource "google_storage_bucket" "bucket" {
   uniform_bucket_level_access = true
 }
 
-# Create Artifact Registry Repository
-resource "google_artifact_registry_repository" "repo" {
+# Create Artifact Registry repository for Docker images
+resource "google_artifact_registry_repository" "iris_docker_repo" {
   project = local.project
   location = local.location
-  repository_id = local.project
+  repository_id = "iris-docker-repo"
   format = "DOCKER"
 }
 
-# Create Cloud Function to compile and run pipeline
-data "archive_file" "pipeline_zip" {
-  type        = "zip"
-  source_dir  = "pipeline_function"
-  output_path = "temp/pipeline_function.zip"
-}
-
-resource "google_storage_bucket_object" "pipeline_obj" {
-  name   = data.archive_file.pipeline_zip.id
-  bucket = google_storage_bucket.bucket.name
-  source = data.archive_file.pipeline_zip.output_path
-}
-
-resource "google_cloudfunctions2_function" "pipeline_function" {
-  name = "pipeline_function"
+# Create Artifact Registry repository for Docker images
+resource "google_artifact_registry_repository" "iris_kfp_repo" {
+  project = local.project
   location = local.location
-  description = "Compile and run pipeline"
+  repository_id = "iris-kfp-repo"
+  format = "KFP"
+}
 
-  build_config {
-    runtime = "python310"
-    entry_point = "main"  # Set the entry point 
-    source {
-      storage_source {
-        bucket = google_storage_bucket.bucket.name
-        object = google_storage_bucket_object.pipeline_obj.name
+# Create Cloud Run to compile and run pipeline
+resource "google_cloud_run_v2_job" "pipeline_job" {
+  name     = "iris-pipeline"
+  location = local.location
+
+  template {
+    template {
+      service_account = google_service_account.account.email
+      containers {
+        image = "${local.location}-docker.pkg.dev/${local.project}/${google_artifact_registry_repository.iris_docker_repo.repository_id}/pipeline_container"
       }
     }
   }
+}
 
-  lifecycle {ignore_changes = [service_config]}
+# Create service account
+resource "google_service_account" "account" {
+  account_id = "iris-sa"
+}
+
+# The binding to grant necessary roles 
+resource "google_project_iam_member" "grant_vertex_permissions" {
+  project = local.project
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_project_iam_member" "grant_storage_permissions" {
+  project = local.project
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.account.email}"
 }
