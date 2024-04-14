db83603cc6626697fca3671bc842cc8650785817
Jake Chen (Google) <jakejchen@google.com>
2024-04-14 23:43:29 +0000

    comments, clarity

--- level2/vertex_train_container/vertex_train.py
+++ level2/vertex_train_container/vertex_train.py
@@ -5,25 +5,22 @@ import google.cloud.aiplatform as aip
 
 print(os.environ)
 
-
-aip.init(
-    project='simple-pipeline-415719',
-    location='us-west1',
-    staging_bucket='simple-pipeline-415719-bucket',
-)
-
+# Define Vertex AI Custom Job
 my_job = aip.CustomJob(
-    display_name='iris_train',
+    display_name="iris_train",
+    project="simple-pipeline-415719",
+    location="us-west1",
+    staging_bucket="simple-pipeline-415719-bucket",
     worker_pool_specs=[
         {
-            "machine_spec":{
+            "machine_spec": {
                 "machine_type": "n1-standard-4",
                 # "accelerator_type": "NVIDIA_TESLA_K80",
                 # "accelerator_count": 1,
             },
             "replica_count": 1,
             "container_spec": {
-                "image_uri": 'us-west1-docker.pkg.dev/simple-pipeline-415719/iris-docker-repo/train_container',
+                "image_uri": "us-west1-docker.pkg.dev/simple-pipeline-415719/iris-docker-repo/train_container",
                 # "command": [],
                 # "args": [],
             },
@@ -31,4 +28,5 @@ my_job = aip.CustomJob(
     ],
 )
 
+# Run Vertex AI Custom Job
 my_job.run()
