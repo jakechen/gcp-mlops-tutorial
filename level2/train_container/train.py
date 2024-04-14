b0e30c6a7d4c0b8bc33d342f3beecd3ced9df837
Jake Chen (Google) <jakejchen@google.com>
2024-04-14 00:54:36 +0000

    fix train image

--- level2/train_container/train.py
+++ level2/train_container/train.py
@@ -11,15 +11,18 @@ iris = load_iris()
 # Train a basic Random Forest model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(iris.data, iris.target)
+print("Model fit complete")
 
 # Save the model to a file
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
+print("Model saved")
 
 # Push saved model to GCS bucket
 storage_client = storage.Client()
-bucket = storage_client.bucket(os.environ['AIP_MODEL_DIR'])
+bucket = storage_client.bucket("simple-pipeline-415719-bucket")
 blob = bucket.blob('model.pkl')
 blob.upload_from_filename('model.pkl')
+print("Model artifact pushed")
 
 print("Training completed.")
