a1bbe0b7ddc30a4be1b6f5b87608c6c6ee8505bf
Jake Chen (Google) <jakejchen@google.com>
2024-04-15 00:02:55 +0000

    comments

--- level2/kfp_run_container/run_pipeline.py
+++ level2/kfp_run_container/run_pipeline.py
@@ -7,10 +7,7 @@ print(os.environ)
 
 
 def main():
-    # Run pipeline
-    aip.init(
-    )
-
+    # Define pipeline
     pipeline_job = aip.PipelineJob(
         display_name="iris",
         template_path="https://us-west1-kfp.pkg.dev/simple-pipeline-415719/iris-kfp-repo/iris/latest",
@@ -20,6 +17,7 @@ def main():
         enable_caching=False # Disable cache for demo purposes; creates new artifact for demo
     )
 
+    # Run pipeline
     pipeline_job.run(
         service_account="iris-sa@simple-pipeline-415719.iam.gserviceaccount.com"
     )
